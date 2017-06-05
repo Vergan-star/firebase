2c3de45b0efa0bf333441e67780b2dfde28b3392
alexandre santos <alexandre@debian-alexandre.NPQD>
2017-06-05 14:12:17 -0300

    comentários das alterações

---
+++ js/app.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB_SymmTz90u9Bj66G3nW4G1OnSeQwyEEI",
+    authDomain: "login-3fefe.firebaseapp.com",
+    databaseURL: "https://login-3fefe.firebaseio.com",
+    projectId: "login-3fefe",
+    storageBucket: "login-3fefe.appspot.com",
+    messagingSenderId: "696526976900"
+  };
+  firebase.initializeApp(config);
+
+  
